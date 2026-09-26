9faadb7fc70659e315d3b27552db63bd82ad9981
eaa25jkni <eaa25jkni@students.eaaa.dk>
2026-09-26 15:39:00 +0200

    Tilføjet aria-labelledby og aria-label + dekroative billeder = alt""
    Tilføjet aria-labelledby til active-filters-section og h2, så en skærmlæserbruger ved hvad sektionen handler om.
    
    Kigget på semantik for gamecars og dialog. Her skiftede jeg bla.a en div til figure.
    Ellers tilføjede jeg aria-label til rating.
    
    Dekorative billeder skal have alt="". Alle ikonerne i game-info er bare dekorative, der slettede jeg deres alt tekster og sat dem til alt=""

--- js/oversigt.js
+++ js/oversigt.js
@@ -288,26 +288,26 @@ function initFilterPanel() {
           const loadingOptimize = index < 4 ? "eager" : "lazy";   //De fire første kort bliver vist above the fold, så de skal ik have lazy loading på. Derfor opstiller vi en betingelse, hvor hvis index er under fire skal loading"eager" på eller skal lazy på.
 
       return `
-          <article class="game-card" tabindex="0">
+          <article class="game-card" tabindex="0" >
                   <img src="${game.image}" alt="Poster of ${game.title}" class="game-poster" loading="${loadingOptimize}" />
-                  <button type="button" class="favorite-btn" data-id="${game.id}" aria-pressed="${isFavorite(game.id)}" aria-label="Favoritknap">
-                      <img src="${favoriteIcon}" alt="FavoritKnap" class="favorite-icon" width="200" height="200" />
+                  <button type="button" class="favorite-btn" data-id="${game.id}" aria-pressed="${isFavorite(game.id)}" aria-label="${game.title} favoritknap">
+                      <img src="${favoriteIcon}" alt="" class="favorite-icon" width="200" height="200" />
                   </button>
                   
               <div class="game-info">
                   <div class="game-card-overskrift">
                       <h3>${game.title} </h3>
-                      <div class="game-rating">
-                          <img src="images/rating-ikon.png" alt="Rating" class="rating-icon">${game.rating}
+                      <div class="game-rating" aria-label="${game.rating} ud af 5">
+                          <img src="images/rating-ikon.png" alt="" class="rating-icon">${game.rating}
                       </div>  
 
                   </div>
 
                   <ul class="info-liste">
                       <li class="game-shelf">Hylde ${game.shelf}</li>
-                      <li class="game-players"><img src="images/antalspillere-ikon.svg" alt="Players" class="players-icon"> ${game.players.min}-${game.players.max} spillere</li>
-                      <li class="game-playtime"><img src="images/spiltid-ikon.svg" alt="Playtime" class="playtime-icon"> ${game.playtime} minutter</li>
-                      <li class="game-genre"><img src="images/genre-ikon.svg" alt="Genre" class="genre-icon"> ${game.genre}</li>
+                      <li class="game-players"><img src="images/antalspillere-ikon.svg" alt="" class="players-icon"> ${game.players.min}-${game.players.max} spillere</li>
+                      <li class="game-playtime"><img src="images/spiltid-ikon.svg" alt="" class="playtime-icon"> ${game.playtime} minutter</li>
+                      <li class="game-genre"><img src="images/genre-ikon.svg" alt="" class="genre-icon"> ${game.genre}</li>
                   </ul>
               </div>
           </article>
@@ -394,12 +394,12 @@ function initFilterPanel() {
       dialogContent.innerHTML = `
       <article class="modal-game-card">
 
-          <div class="game-poster-container">
+          <figure class="game-poster-container">
               <img src="${game.image}" alt="Poster of ${game.title}" class="game-poster" />
               <button type="button" class="favorite-btn" data-id="${game.id}" aria-pressed="${isFavorite(game.id)}" aria-label="Favoritknap">
                           <img src="${favoriteIconSrc}" alt="FavoritKnap" class="favorite-icon"/>
               </button>
-          </div>
+          </figure>
 
           <div class="dialog-game-info">
               <h1>${game.title} </h1>
@@ -407,12 +407,12 @@ function initFilterPanel() {
               <p class="game-shelf">Hylde ${game.shelf}</p>
 
               <ul class="game-icons-grid">
-                  <li class="game-genre"><img src="images/genre-ikon.svg" alt="Genre" class="genre-icon"> ${game.genre}</li>
-                  <li class="game-rating"><img src="images/rating-ikon.png" alt="Rating" class="rating-icon"> ${game.rating}</li>
-                  <li class="game-players"><img src="images/antalspillere-ikon.svg" alt="Players" class="players-icon"> ${game.players.min}-${game.players.max} spillere</li>
-                  <li class="game-playtime"><img src="images/spiltid-ikon.svg" alt="Playtime" class="playtime-icon"> ${game.playtime} minutter</li>
-                  <li class="game-age"><img src="images/alder-ikon.svg" alt="Age" class="age-icon"> ${game.age}+</li>
-                  <li class="game-difficulty"><img src="images/grad-ikon.svg" alt="Difficulty" class="difficulty-icon"> ${game.difficulty}</li>
+                  <li class="game-genre"><img src="images/genre-ikon.svg" alt="" class="genre-icon"> ${game.genre}</li>
+                  <li class="game-rating"><img src="images/rating-ikon.png" alt="" class="rating-icon"> ${game.rating}</li>
+                  <li class="game-players"><img src="images/antalspillere-ikon.svg" alt="" class="players-icon"> ${game.players.min}-${game.players.max} spillere</li>
+                  <li class="game-playtime"><img src="images/spiltid-ikon.svg" alt="" class="playtime-icon"> ${game.playtime} minutter</li>
+                  <li class="game-age"><img src="images/alder-ikon.svg" alt="" class="age-icon"> ${game.age}+</li>
+                  <li class="game-difficulty"><img src="images/grad-ikon.svg" alt="" class="difficulty-icon"> ${game.difficulty}</li>
               </ul>
 
               <p class="game-rules">${game.rules}</p>
